6c9c0bcb816426d77c2b230207a6c99b926d67e9
Alex Ullrich <alex.ullrich@gmail.com>
2013-10-16 07:46:27 -0400

    try using a nancy bootstrapper for bundle creation

---
+++ SquishIt.NancySample/SquishItBootstrapper.cs
@@ -0,0 +1,16 @@
+﻿using Nancy;
+using SquishIt.Framework;
+
+namespace SquishIt.NancySample
+{
+    public class SquishItBootstrapper : DefaultNancyBootstrapper
+    {
+        protected override void ApplicationStartup(Nancy.TinyIoc.TinyIoCContainer container, Nancy.Bootstrapper.IPipelines pipelines)
+        {
+            Bundle.JavaScript()
+                .Add("~/Content/js/js1.js")
+                .Add("~/Content/js/js2.js")
+                .AsCached("hello", "~/assets/js/hello");
+        }
+    }
+}

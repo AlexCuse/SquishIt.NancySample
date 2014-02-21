7338026d4d425960151978171596749066b460bc
Alex Ullrich <alex.ullrich@gmail.com>
2014-02-20 21:16:06 -0500

    update nancy version to 0.22.2 - this required updating SquishIt to 0.9.5.1-alpha1 - 0.9.5.1 will be first version supporting pluggable path translators,   which allow path resolution without HttpContext (which is not available   in newer versions of nancy)

---
+++ SquishIt.NancySample/NancyPathTranslator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Nancy;
+using SquishIt.Framework;
+
+namespace SquishIt.NancySample
+{
+    public class NancyPathTranslator : IPathTranslator
+    {
+        private readonly IRootPathProvider _rootPathProvider;
+
+        public NancyPathTranslator(IRootPathProvider rootPathProvider)
+        {
+            _rootPathProvider = rootPathProvider;
+        }
+
+        public string ResolveAppRelativePathToFileSystem(string file)
+        {
+            // Remove query string
+            if(file.IndexOf('?') != -1)
+            {
+                file = file.Substring(0, file.IndexOf('?'));
+            }
+
+            return _rootPathProvider.GetRootPath() + "/" + file.TrimStart('~').TrimStart('/');
+        }
+
+        public string ResolveFileSystemPathToAppRelative(string file)
+        {
+            var root = new Uri(_rootPathProvider.GetRootPath());
+            return root.MakeRelativeUri(new Uri(file, UriKind.RelativeOrAbsolute)).ToString();
+        }
+    }
+}
--- SquishIt.NancySample/SquishItBootstrapper.cs
+++ SquishIt.NancySample/SquishItBootstrapper.cs
@@ -1,4 +1,5 @@
 ﻿using Nancy;
+using Nancy.Hosting.Aspnet;
 using SquishIt.Framework;
 
 namespace SquishIt.NancySample
@@ -7,6 +8,9 @@ namespace SquishIt.NancySample
     {
         protected override void ApplicationStartup(Nancy.TinyIoc.TinyIoCContainer container, Nancy.Bootstrapper.IPipelines pipelines)
         {
+            Bundle.ConfigureDefaults()
+                .UsePathTranslator(new NancyPathTranslator(new AspNetRootSourceProvider()));
+
             Bundle.JavaScript()
                 .Add("~/Content/js/js1.js")
                 .Add("~/Content/js/js2.js")
